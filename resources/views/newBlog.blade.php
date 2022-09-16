7e00db68a0bbff983e5cc3a6e35c3eaa0fee430c
Edoardo Tarulli <S1094923@studenti.univpm.it>
2022-09-16 21:25:38 +0200

    Piccoli miglioramenti

--- resources/views/newBlog.blade.php
+++ resources/views/newBlog.blade.php
@@ -3,15 +3,15 @@
 @section('title', 'Nuovo Blog')
 
 @section('content')
+<hr class="spaziaturahr">
 <div>
-    
-    <div class="wrap-form-main-space">
+    <div style="text-align: center">
             {{ Form::open(array('route' => 'newBlog', 'class' => 'form')) }}
-            <h1>Inizia subito un nuovo blog<h1>
-      
+            <h1>Inizia subito un nuovo blog!<h1>
+            <hr class="spaziaturahr">
              <div  class="wrap-element-form">
-                {{ Form::label('tema', 'Inserisci il tema del vlog', ['class' => 'label-form']) }}
-                <p class="subtitleform">Di cosa parlerà il tuo blog?</p>
+                {{ Form::label('tema', 'Inserisci il tema del blog', ['class' => 'label-form']) }}
+                <p class="subtitleform">Di cosa parlerà?</p>
                 {{ Form::text('tema', '', ['class' => 'input-form','id' => 'tema', 'placeholder'=> 'Tema del blog']) }}
                 @if ($errors->first('tema'))
                 <ul class="errors">
@@ -21,8 +21,7 @@
                 </ul>
                 @endif
             </div>
-
-
+            <hr class="spaziaturahr">
             <div  class="wrap-element-form">
                 {{ Form::label('messaggio', 'Primo messaggio', ['class' => 'label-form']) }}
                 <p class="subtitleform">Qual'è la tua opinione?</p>
@@ -35,12 +34,13 @@
                 </ul>
                 @endif
             </div>
-            
+            <hr class="spaziaturahr">
             <div class="container-form-button">                
-                {{ Form::submit('Crea', ['class' => 'form-button']) }}
+                {{ Form::submit('Crea', ['class' => 'form-button'])}}
             </div>
-            
             {{ Form::close() }}
         </div>
+    </div>    
 </div>
+
 @endsection
