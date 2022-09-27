b82c7a7e736b4fbbad5d5dd6369c282a3d16c42a
luca1 <s1093754@studenti.univpm.it>
2022-09-27 17:42:08 +0200

    correzione rotta

--- resources/views/gestioneStaf.blade.php
+++ resources/views/gestioneStaf.blade.php
@@ -3,17 +3,6 @@
 @section('title', 'Gestione Staf')
 
 @section('content')
-<<<<<<< HEAD
-<hr class="spaziaturahr">
-<div  style="text-align: center; font-size: large">
-    <div>
-        <h1>In questa pagina potrai gestire il tuo staff!</h1>
-        <br>
-        <br>
-        <h2>Di seguito è riportato l'elenco degli utenti che fanno parte dello staff del sito <br><br>
-    </div>
-    
-=======
 <div>
    
     spazio
@@ -23,7 +12,6 @@
     <div ><a href="{{ route('nuovoStaf') }}" class="highlight" >Aggiungi  un membro allo staf</a></div>
 
     <div>Membri dello staf</div>
->>>>>>> 2549029fff2647ec46b3d737bed74d622c172778
 
     @isset($staf)
     @if(count($staf)===0)
@@ -42,7 +30,7 @@
     <hr class="spaziaturahr">
     <div>
         <h3>Se desideri aggiungere un nuovo membro allo staff puoi farlo qui: <br> <br>
-        <a href="{{ route('staf') }}" class="highlight" >Aggiungi  un membro allo staff</a>
+        <a href="{{ route('nuovoStaf') }}" class="highlight" >Aggiungi  un membro allo staff</a>
     </div>
     
         
