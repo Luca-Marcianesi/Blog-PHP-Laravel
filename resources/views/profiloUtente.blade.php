6f5cc7a58f26146ee8d4a0536b99f09eb8a20fcc
luca1 <s1093754@studenti.univpm.it>
2022-10-11 11:45:44 +0200

    modifiche di viste

--- resources/views/profiloUtente.blade.php
+++ resources/views/profiloUtente.blade.php
@@ -7,64 +7,60 @@
    
 
     @isset($utente)
-    
-
-
-  
-    <div class="main_element" style="text-align: center; font-size: large">
-        <h1>Dati anagrafici</h1>
-        <hr class="spaziaturahr">
-        <div >Nome: {{$utente->name}}</div><br>
-        <div>Cognome: {{$utente->surname}}</div><br>
-        <div>E-mail: {{$utente->email}}</div><br>
-        <div>Data di Nascita: {{$utente->data_nascita}}</div><br>
-        <div>Biografia: {{$utente->descrizione}}</div>
-    </div>
-
-    <hr class="spaziaturahr">
-    @can('isFriend',$utente->id)
-        <div style="text-align: center; font-size: large">Attualmente siete amici</div>
-        <hr class="spaziaturahr">
-        @isset($blogs)
-        @if(@count($blogs)===0)
-            <div style="text-align: center; font-size: large">
-                Attualmente non ci sono blog pubblicati
-            </div>
+        <div class="main_element" style="text-align: center; font-size: large">
             
-        
-        @endif()  
-         
-        
-        @foreach($blogs as $blog)
-        
+            <p class="titolo">Dati anagrafici</p>
+            
+            <div >Nome: {{$utente->name}}</div><br>
+            <div>Cognome: {{$utente->surname}}</div><br>
+            <div>E-mail: {{$utente->email}}</div><br>
+            <div>Data di Nascita: {{$utente->data_nascita}}</div><br>
+            <div>Biografia: {{$utente->descrizione}}</div>
+        </div>
+
+        <hr class="spaziaturahr">
+
         <div style="text-align: center; font-size: large">
-            <a href="{{ route('blog',$blog->id) }}" class="highlight" >Tema: {{$blog->tema}}</a> 
-        </div>
-        <br>
-        @endforeach
-        @endisset()
-    @endcan
+            @can('isFriend',$utente->id)
 
-        
-    @can('isRifiutata',$utente->id)
-        <div style="text-align: center; font-size: large"> 
-            Amicizia rifiutata <br>
-            <a href="{{ route('inviaAmicizia',$utente->id) }}" title="richiesta">Invia di nuovo</a>
-        </div>
-    @endcan
+                <p class="sotto-titolo">Attualmente siete amici</p>
+            
+                <hr class="spaziaturahr">
+                
+                @isset($blogs)
 
-    @can('isSospesa',$utente->id)
-        <div style="text-align: center; font-size: large">Amicizia in attesa di risposta</div>
-    @endcan
+                    @if(@count($blogs)===0)
+                        <p>Attualmente non ci sono blog pubblicati</p> 
+                    @endif()  
+                    
+                    
+                    @foreach($blogs as $blog)
+                        <div class="blog-link">
+                            <a href="{{ route('blog',$blog->id) }}" >Tema: {{$blog->tema}}</a> 
+                        </div>
+                        <hr class="spaziaturahr">
+                        
+                    @endforeach
+                @endisset()
+            @endcan
 
-    @can('richiedereAmicizia',$utente->id)
-        <div style="text-align: center; width: auto; height: auto">
-            <div>Devi far parte del gruppo di amici per visualizzare i blog </div>
-            <a href="{{ route('inviaAmicizia',$utente->id) }}" title="richiesta">Invia richiesta</a>
+            
+            @can('isRifiutata',$utente->id)
+                <p>Amicizia rifiutata <br>
+                <a href="{{ route('inviaAmicizia',$utente->id) }}" title="richiesta">Invia di nuovo</a></p>
+            @endcan
+
+            @can('isSospesa',$utente->id)
+                <p>Amicizia in attesa di risposta</p>
+            @endcan
+
+            @can('richiedereAmicizia',$utente->id)
+                <p>Devi far parte del gruppo di amici per visualizzare i blog 
+                <a href="{{ route('inviaAmicizia',$utente->id) }}" title="richiesta">Invia richiesta</a>
+                </p>
+            @endcan
         </div>
-    @endcan
     @endisset()
-    
     
 </div>
 @endsection
