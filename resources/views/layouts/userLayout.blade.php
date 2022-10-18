56aa9a08687b40c17de5563acfedd48e67f61d96
luca1 <s1093754@studenti.univpm.it>
2022-10-18 17:01:42 +0200

    bottone modifica link

--- resources/views/layouts/userLayout.blade.php
+++ resources/views/layouts/userLayout.blade.php
@@ -8,6 +8,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script> 
         <script src='https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.2/bootbox.min.js'></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
         <script defer src="/js/popup.js"></script>
         @show
         @section('scripts')
